4468fc3efe834c5a28235c183779c9d7151e4c3b
Adrian <adriferreiro.88@gmail.com>
2023-08-28 11:17:03 -0300

    Autoguardado y vista previa recuperando del localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,53 @@
 import './App.css';
-import {useQuill} from 'react-quilljs';
+import { useQuill } from 'react-quilljs';
 import 'quill/dist/quill.snow.css';
 import toolbar from './toolbar';
+import { useEffect } from 'react'; // Importa useEffect
 import { Link } from 'react-router-dom';
 
 function App() {
+  const { quill, quillRef } = useQuill({
+    modules: {
+      toolbar: toolbar,
+      
+    },
+  });
 
+  useEffect(() => {
+    if (quill) {
 
-   const {quill, quillRef} = useQuill({modules:{
-      toolbar: toolbar
-   }});
+      // Cargar contenido guardado desde el Local Storage al editor
+      const savedContent = localStorage.getItem('editorContent');
+      if (savedContent) {
+        quill.root.innerHTML = savedContent;
+      }
 
+      quill.on('text-change', () => {
+        const editorContent = quill.root.innerHTML;
+        console.log('Contenido del editor:', editorContent);
 
+        // Guardar el contenido en el Local Storage
+        localStorage.setItem('editorContent', editorContent);
+      });
 
-  
+      // quill.getModule('toolbar').addHandler('print', () => {
+      //   const editorContent = quill.root.innerHTML;
+      //   console.log('Contenido del editor para impresión:', editorContent);
+      //   window.print(); // Inicia la impresión del navegador
+      // });
+    }
+  }, [quill]);
+
   return (
     <>
-      <h1> Quill </h1>
-      <h3> Editor de texto </h3>
-      <div   ref={quillRef} ></div>
-      <Link to={'/vistaPrevia'} >
+      <h1>Quill</h1>
+      <h3>Editor de texto</h3>
+      <div ref={quillRef}></div>
+      <Link to={'/vistaPrevia'}>
         <button> Vista Previa </button>
       </Link>
-      
     </>
-  )
+  );
 }
 
 export default App
